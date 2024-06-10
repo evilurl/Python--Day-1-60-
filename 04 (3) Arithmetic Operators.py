730a2ecdfb3fbc3ad681f564e7b3b5cc6f86d931
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-06-10 22:22:13 +0500

    Update 04 (3) Arithmetic Operators.py

---
+++ 04 (3) Arithmetic Operators.py
@@ -0,0 +1,10 @@
+# Operators are used to perform operations on operands(variables and values).
+
+# List of Arithmetic Operators:
+# 1. Addition & Unary Plus (+)
+# 2. Subtraction & Unary Minus (-)
+# 3. Multiplication (*)
+# 4. Division (/)
+# 5. Floor Division (//)
+# 6. Modulus (%)
+# 7. Power (**)
