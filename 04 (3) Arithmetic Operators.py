0fb390491cc08a0bc750ac0871b84c9005d99745
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-06-11 17:19:46 +0500

    Update 04 (3) Arithmetic Operators.py

--- 04 (3) Arithmetic Operators.py
+++ 04 (3) Arithmetic Operators.py
@@ -8,3 +8,92 @@
 # 5. Floor Division (//)
 # 6. Modulus (%)
 # 7. Power (**)
+
+
+###### 1. Addition & Unary Plus (+) ######
+# Unary operator is applied on a single operand
+# Addition operator is applied on two operands.
+
+# For example;
+x = -10
+y = +(x) # Unary Plus Operator
+print(y)
+
+x = 10
+y = 20
+z = (x + y) # Addition Operator
+print(z)
+
+###### 2. Subtraction & Unary Minus Plus (-) ######
+# For example;
+x = -10
+y = -(x) # Unary Minus Operator
+print(y)
+
+x = 10
+y = 20
+z = (x - y) # Subtraction Operator
+print(z)
+
+###### 3. Multiplication (*) ######
+# - Binary Operator
+# - Used to multiply two operands
+
+# For example;
+
+x = 10
+y = 20
+z = (x * y) # Multiplication Operator
+print(z)
+
+###### 4. Division Operator (/) ######
+# - Binary Operator
+# - Used to divide two operands and give result in floating points.
+
+# For example;
+
+x = 10
+y = 20
+z = (x / y) # Divison Operator
+w = (y / x)
+print(z)
+print(w)
+
+###### 5. Floor Operator (//) ######
+# - Binary Operator
+# - Used to divide two operands but the result will always be an integer value.
+
+# For example;
+
+x = 10
+y = 20
+z = (x // y) # Floor Operator
+w = (y // x)
+print(z)
+print(w)
+
+###### 6. Modulus (%) ######
+# - Binary Operator
+# - Performs division, but it return the remainder as a result
+
+# For example;
+
+x = 10
+y = 20
+z = (x % y) # Modulus 
+w = (y % x)
+print(z)
+print(w)
+
+###### 7. Power (**) ######
+# - Binary Operator
+# - Calculates the first operanf raised to the power of second operand
+
+# For example;
+
+x = 10
+y = 20
+z = (x ** y) # Power 
+w = (y ** x)
+print(z)
+print(w)
